235ac0dc0fb1728e31f871ca82252f7f52904fa3
khushi28v <127029434+khushi28v@users.noreply.github.com>
2025-05-01 20:10:30 +0530

    Add listings/consumers.py 1746110429906

---
+++ listings/consumers.py
@@ -0,0 +1,80 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
+from channels.db import database_sync_to_async
+from .models import ChatMessage, Notification
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'chat_room'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data.get('message')
+        username = data.get('username')
+
+        if not message or not username:
+            return
+
+        user = await self.get_user(username)
+        chat_message = await self.create_chat_message(user, message)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'timestamp': chat_message.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
+
+    @database_sync_to_async
+    def get_user(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+
+    @database_sync_to_async
+    def create_chat_message(self, user, message):
+        if user:
+            return ChatMessage.objects.create(user=user, message=message)
+        else:
+            return None
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
+        if self.user.is_anonymous:
+            await self.close()
+        else:
+            self.group_name = f'notifications_{self.user.id}'
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def send_notification(self, event):
+        await self.send(text_data=json.dumps(event['notification']))
